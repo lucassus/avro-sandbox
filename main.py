60ced5fed9f7fb4d2cc7d9b0d34fb88f494740c0
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2025-03-26 22:41:33 +0100

    Replace Avro with Fastavro for data serialization and deserialization

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import avro.schema
-from avro.datafile import DataFileReader, DataFileWriter
-from avro.io import DatumReader, DatumWriter
+from fastavro.write import writer
+from fastavro.read import reader
+from fastavro.schema import load_schema
 
 if __name__ == "__main__":
-    schema = avro.schema.parse(open("user.avsc", "rb").read())
+    schema = load_schema("user.avsc")
 
-    writer = DataFileWriter(open("users.avro", "wb"), DatumWriter(), schema)
-    writer.append({"name": "Alyssa", "favorite_number": 256})
-    writer.append({"name": "Ben", "favorite_number": 7, "favorite_color": "red"})
-    writer.close()
+    with open("users.avro", "wb") as out:
+        writer(
+            out,
+            schema,
+            [
+                {"name": "Alyssa", "favorite_number": 256},
+                {"name": "Ben", "favorite_number": 7, "favorite_color": "red"},
+            ],
+        )
 
-    reader = DataFileReader(open("users.avro", "rb"), DatumReader())
-    for user in reader:
-        print(user)
-    reader.close()
+    with open("users.avro", "rb") as fo:
+        for record in reader(fo):
+            print(record)
