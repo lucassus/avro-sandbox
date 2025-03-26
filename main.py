2a2239f0a5560eb770355efb855a6d373c993421
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2025-03-26 21:36:49 +0100

    Implement Avro data serialization and deserialization with user schema

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from avro-sandbox!")
-
+import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
 
 if __name__ == "__main__":
-    main()
+    schema = avro.schema.parse(open("user.avsc", "rb").read())
+
+    writer = DataFileWriter(open("users.avro", "wb"), DatumWriter(), schema)
+    writer.append({"name": "Alyssa", "favorite_number": 256})
+    writer.append({"name": "Ben", "favorite_number": 7, "favorite_color": "red"})
+    writer.close()
+
+    reader = DataFileReader(open("users.avro", "rb"), DatumReader())
+    for user in reader:
+        print(user)
+    reader.close()
