76e62ff4e27cbc1a59f1b11001f16e6fb6be47b0
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2025-03-27 11:32:01 +0100

    Add Address schema and update User schema to include primary_address

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-from fastavro.write import writer
 from fastavro.read import reader
 from fastavro.schema import load_schema
+from fastavro.write import writer
 
 if __name__ == "__main__":
-    schema_v1 = load_schema("user_v1.avsc")
-    schema_v2 = load_schema("user_v2.avsc")
+    schema = load_schema("schemas/sandbox.User.avsc")
 
     with open("users.avro", "wb") as out:
         writer(
             out,
-            schema_v1,
+            schema,
             [
                 {"name": "Alyssa", "favorite_number": 256},
                 {"name": "Ben", "favorite_number": 7, "favorite_color": "red"},
+                {
+                    "name": "Anna",
+                    "favorite_number": 3,
+                    "primary_address": {
+                        "street": "123 Main St",
+                        "city": "San Francisco",
+                        "state": "CA",
+                        "zip": "94105",
+                    },
+                },
             ],
         )
 
     with open("users.avro", "rb") as fo:
-        for record in reader(fo, schema_v2):
+        for record in reader(fo, schema):
             print(record)
